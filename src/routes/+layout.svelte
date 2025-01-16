c262b1d0d4c7abf8c14240d119765fd1370a7b4e
Aryxst <jessieodeh08@gmail.com>
2025-01-16 19:28:38 +0100

    style(css): resize sidebar
    chore: update imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
  import { dev } from "$app/environment";
  import AppSidebar from "@/components/app-sidebar.svelte";
+ import BottomPanel from "@/components/bottom-panel.svelte";
  import Header from "@/components/header";
  import * as Sidebar from "@/components/ui/sidebar";
+ import { GoFileAPI } from "@/services/hosters/gofile";
  import { events } from "@/specta-bindings";
- import { gamesStore } from "@/store/games.store";
- import { settingsStore } from "@/store/settings.store";
+ import { games, settings } from "@/stores";
  import { ModeWatcher } from "mode-watcher";
  import { onMount } from "svelte";
  import { Toaster } from "svelte-sonner";
@@ -14,28 +15,35 @@
  let { children } = $props();
 
  onMount(async () => {
-  events.executableStarted.listen(event => {
-   gamesStore.updateGame(["executablePath", event.payload.path], state => ({
+  events.executableStartedEvent.listen(async ({ payload }) => {
+   GoFileAPI.authorize().then(token => {
+    GoFileAPI.getDownloadLink("4ZInBP").then(url => {});
+   });
+
+   games.updateGame(["executablePath", payload.path], state => ({
     ...state,
     running: true,
    }));
   });
 
-  events.executableFinished.listen(event => {
-   gamesStore.updateGame(["executablePath", event.payload.path], state => ({
+  events.executableFinishedEvent.listen(({ payload }) => {
+   games.updateGame(["executablePath", payload.path], state => ({
     ...state,
     lastPlayedAt: Date.now(),
-    playtimeInSeconds: state.playtimeInSeconds + event.payload.execution_time,
+    playtimeInSeconds: state.playtimeInSeconds + payload.execution_time,
     running: false,
    }));
   });
  });
 </script>
 
-<Toaster theme={$settingsStore.theme} />
+<Toaster theme={$settings.theme} />
 <ModeWatcher />
 
-<Sidebar.Provider>
+<BottomPanel />
+<Sidebar.Provider
+ style="--sidebar-width: 14rem; --sidebar-width-mobile: 20rem;"
+>
  <AppSidebar />
  <main class="w-full">
   <Header />
