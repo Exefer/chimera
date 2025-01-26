2924c2227f8f889818d94a5e82e543e082aabaf5
Aryxst <jessieodeh08@gmail.com>
2025-01-26 12:23:14 +0100

    fix: use sveltekit-superforms instead of felte causing state_unsafe_mutation error
    refactor: improve form validation and code readability

--- src/routes/settings/sources.svelte
+++ src/routes/settings/sources.svelte
@@ -2,7 +2,7 @@
  import { z } from "zod";
 
  export const schema = z.object({
-  url: z.string().url(),
+  url: z.string().url("Please enter a valid URL"),
  });
 
  export type FormSchema = typeof schema;
@@ -11,64 +11,58 @@
 <script lang="ts">
  import { Button, buttonVariants } from "@/components/ui/button";
  import * as Dialog from "@/components/ui/dialog";
+ import * as Form from "@/components/ui/form";
  import { Input } from "@/components/ui/input";
- import { Label } from "@/components/ui/label";
  import { Separator } from "@/components/ui/separator";
  import { sources } from "@/stores";
  import * as Types from "@/types";
- import { validator } from "@felte/validator-zod";
- import { createForm } from "felte";
  import CircleMinus from "lucide-svelte/icons/circle-minus";
  import CirclePlus from "lucide-svelte/icons/circle-plus";
  import RefreshCcw from "lucide-svelte/icons/refresh-ccw";
- import { toast } from "svelte-sonner";
- import { slide } from "svelte/transition";
+ import { fade, slide } from "svelte/transition";
+ import { zodClient } from "sveltekit-superforms/adapters";
+ import { superForm } from "sveltekit-superforms/client";
 
  let temporarySource = $state<Types.Source | null>(null);
 
- const { form, errors } = createForm<z.infer<FormSchema>>({
-  onSubmit: async values => {
-   const response = await fetch(values.url);
-
-   if (response.ok) {
-    if ($sources.find(source => source.url == values.url)) {
-     throw "Source already added!";
+ const form = superForm(
+  { url: "" },
+  {
+   SPA: true,
+   validators: zodClient(schema),
+   onSubmit: async () => {
+    const url = $formData.url;
+    if ($sources.find(source => source.url == url)) {
+     $errors.url = ["Source already added!"];
+     return;
     }
-    return await response
-     .json()
-     .then(source => ({ ...source, url: values.url }))
+    fetch(url)
+     .then(response => response.json())
+     .then(source => {
+      temporarySource = { ...source, url };
+     })
      .catch(() => {
-      throw "Not a valid JSON file";
+      $errors.url = ["Not a valid JSON file"];
      });
-   }
+   },
   },
-  onSuccess: response => {
-   temporarySource = response as Types.Source;
-  },
-  onError(error, context) {
-   context.setErrors(
-    "url",
-    error instanceof Error ? error.message : (error as string),
-   );
-  },
-  extend: [validator({ schema })],
- });
+ );
+ const { form: formData, errors, enhance } = form;
 </script>
 
 <h1 class="text-2xl font-bold">Sources</h1>
 
 <p>
- Chimera will fetch the download links from these sources. The source URL must
- be a direct link to a .json file containing the download links.
+ Chimera will fetch the download links from these sources. The source URL must be a direct
+ link to a .json file containing the download links.
 </p>
 
 <div class="flex justify-between">
  <Button
   variant="outline"
-  disabled={!$sources.length}
+  disabled={$sources.length == 0}
   onclick={async () => {
    await sources.refreshSources();
-   toast.success("All download sources are synced!");
   }}><RefreshCcw />Sync Sources</Button
  >
  <Dialog.Root>
@@ -82,15 +76,19 @@
    </Dialog.Header>
    <Separator />
    <div class="space-y-4">
-    <form class="space-y-2" use:form>
-     <Label class="text-muted-foreground" for="url">Download Source URL</Label>
-     <div class="flex gap-4">
-      <Input type="url" name="url" autocomplete="off" />
-      <Button type="submit" variant="outline">Validate</Button>
-     </div>
-     {#if $errors.url}
-      <span class="mt-1 text-sm text-red-500">{$errors.url}</span>
-     {/if}
+    <form method="POST" use:enhance>
+     <Form.Field {form} name="url">
+      <Form.Control>
+       {#snippet children({ props })}
+        <Form.Label>Download Source URL</Form.Label>
+        <div class="flex gap-2">
+         <Input {...props} bind:value={$formData.url} autocomplete="off" />
+         <Form.Button variant="outline">Validate</Form.Button>
+        </div>
+       {/snippet}
+      </Form.Control>
+      <Form.FieldErrors />
+     </Form.Field>
     </form>
 
     {#if temporarySource}
@@ -99,12 +97,12 @@
        <p>{temporarySource.name}</p>
        <small>Found {temporarySource.downloads.length} download options</small>
       </div>
-      <Button
+      <Dialog.Close
+       class={buttonVariants()}
        onclick={() => {
         sources.addSource(temporarySource!);
         temporarySource = null;
-        toast.success("Added download source");
-       }}>Import</Button
+       }}>Import</Dialog.Close
       >
      </div>
     {/if}
@@ -115,7 +113,7 @@
 
 <ul class="space-y-4">
  {#each $sources as source, index (source.name)}
-  <li class="space-y-2 rounded-lg border p-4 shadow-sm" out:slide>
+  <li class="space-y-2 rounded-lg border p-4 shadow-sm" out:fade in:slide>
    <h1 class="text-lg font-bold">{source.name}</h1>
    <small>{source.downloads.length} download options</small>
    <p class="text-sm text-muted-foreground">Download Source URL</p>
@@ -125,7 +123,6 @@
      variant="outline"
      onclick={() => {
       sources.removeSource(index);
-      toast.success("Removed download source");
      }}><CircleMinus />Remove</Button
     >
    </div>
