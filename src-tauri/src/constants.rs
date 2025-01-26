506e2689941e8b41101b2d460b10a9fd3d0dd22b
Aryxst <jessieodeh08@gmail.com>
2025-01-26 12:16:11 +0100

    feat: add default user agent and adjust progress event frequency

--- src-tauri/src/constants.rs
+++ src-tauri/src/constants.rs
@@ -1,1 +1,6 @@
-pub const PROGRESS_EVENT_SKIP: u32 = 2_500;
+//! This module contains constants used throughout the application.
+
+/// The default user agent used for HTTP requests.
+pub const DEFAULT_USER_AGENT: &str = "chimera";
+/// Only sends a progress event every `x` iterations of the download loop to reduce overhead
+pub const PROGRESS_EVENT_SKIP: u32 = 5000;
