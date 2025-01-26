c4374ffbf4e1dc942bd936a6c4f883a41f9ef466
Aryxst <jessieodeh08@gmail.com>
2025-01-26 12:44:24 +0100

    refactor: initialize apps and appsByLetter stores with steam data and strip tests

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,63 +1,33 @@
 <script lang="ts">
  import "$lib/i18n";
- import { Button } from "@/components/ui/button";
- import { appList, downloads } from "@/stores";
+ import { apps, appsByLetter } from "@/stores";
  import * as Steam from "@/types/steam.types";
- import { formatBytes } from "@/utils";
+ import { formatTitle } from "@/utils";
  import ky from "ky";
  import { onMount } from "svelte";
- import { toast } from "svelte-sonner";
 
- /* TESTING */
- const TEST_URL =
-  "https://releases.ubuntu.com/24.04.1/ubuntu-24.04.1-desktop-amd64.iso";
- /* END OF TESTING */
+ // TODO: Use some kind of database to reduce strings size on heap
  onMount(async () => {
-  await ky<Steam.App[]>(
-   atob(
-    "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2h5ZHJhbGF1bmNoZXIvaHlkcmEvcmVmcy9oZWFkcy9tYWluL3NlZWRzL3N0ZWFtLWdhbWVzLmpzb24",
-   ),
-  )
-   .then(response => response.json())
-   .then(data => appList.set(data));
+  if (!$apps || !appsByLetter) {
+   await ky<Steam.App[]>(
+    atob(
+     "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2h5ZHJhbGF1bmNoZXIvaHlkcmEvcmVmcy9oZWFkcy9tYWluL3NlZWRzL3N0ZWFtLWdhbWVzLmpzb24",
+    ),
+   )
+    .then(response => response.json())
+    .then(data => {
+     apps.set(data);
+     appsByLetter.set(
+      data.reduce<Record<string, { name: string; id: number }[]>>((acc, app) => {
+       if (!app.name) return acc;
+       const formattedTitle = formatTitle(app.name);
+       const [firstLetter] = formattedTitle;
+       if (!acc[firstLetter]) acc[firstLetter] = [];
+       acc[firstLetter].push({ name: formattedTitle, id: app.id });
+       return acc;
+      }, {}),
+     );
+    });
+  }
  });
 </script>
-
-<!-- TESTING -->
-<div class="p-4">
- <Button
-  onclick={() => {
-   downloads.addDownload(TEST_URL, "69420", "Scary App!1!", "test.zip");
-   toast("Download started!");
-  }}>Start</Button
- >
- <Button
-  onclick={() => {
-   downloads.abortDownload(TEST_URL);
-   const target = $downloads.findIndex(download => download.url == TEST_URL);
-   if (target != -1) $downloads.splice(target, 1);
-   toast("Download aborted");
-  }}>Abort</Button
- >
- <Button
-  onclick={() => {
-   downloads.pauseDownload(TEST_URL);
-   toast("Download paused!");
-  }}>Pause</Button
- >
-
- <Button
-  onclick={async () => {
-   const download = $downloads[0];
-
-   downloads.resumeDownload(
-    download.url,
-    download.path!,
-    download.downloaded_bytes!,
-   );
-   toast(`Download resumed from ${formatBytes(download.downloaded_bytes!)}!`);
-  }}>Resume</Button
- >
- <pre class="max-w-xs">{JSON.stringify($downloads, null, 2)}</pre>
-</div>
-<!-- END OF TESTING -->
