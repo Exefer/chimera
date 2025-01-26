1b24159123cc4ad01e9947f4fe78a20cfcdcec94
Aryxst <jessieodeh08@gmail.com>
2025-01-26 12:19:16 +0100

    refactor: improve download event handling and simplify download commands
    refactor: use enums for events

--- src-tauri/src/http_downloader.rs
+++ src-tauri/src/http_downloader.rs
@@ -1,10 +1,15 @@
-use crate::constants::PROGRESS_EVENT_SKIP;
-use crate::{
-    AppState, DownloadAbortedEvent, DownloadCompletedEvent, DownloadPausedEvent,
-    DownloadProgressEvent, DownloadStartedEvent, Error,
-};
+//! This module contains the logic for downloading files over HTTPS with commands exposed to the frontend.
+//!
+//! It provides functionality for:
+//! - Downloading files with progress tracking
+//! - Pausing/resuming downloads
+//! - Aborting downloads
+//! - Custom headers
+use crate::constants::DEFAULT_USER_AGENT;
+use crate::{constants::PROGRESS_EVENT_SKIP, AppState, Error};
 use futures::StreamExt;
-use log::info;
+use serde::{Deserialize, Serialize};
+use specta::Type;
 use std::{path::Path, time::Instant};
 use tauri::State;
 use tauri::{http::HeaderName, AppHandle};
@@ -17,9 +22,35 @@ use tauri_specta::Event;
 use tokio::io::{AsyncWriteExt, BufWriter};
 use tokio::sync::Mutex;
 
-fn parse_headers(custom_headers: Option<Vec<(&str, &str)>>) -> HeaderMap {
+#[derive(Serialize, Deserialize, Debug, Clone, Type, Event)]
+#[serde(rename_all = "lowercase", tag = "type", content = "data")]
+pub enum DownloadEvent {
+    Started {
+        url: String,
+        path: String,
+        content_length: u64,
+    },
+    Progress {
+        url: String,
+        progress_percentage: f64,
+        downloaded_bytes: u64,
+        download_speed: u64,
+        eta: u64,
+    },
+    Paused {
+        url: String,
+    },
+    Completed {
+        url: String,
+    },
+    Aborted {
+        url: String,
+    },
+}
+
+fn parse_headers(custom_headers: Option<Vec<(String, String)>>) -> HeaderMap {
     let mut headers = HeaderMap::new();
-    headers.insert(USER_AGENT, "chimera".parse().unwrap());
+    headers.insert(USER_AGENT, DEFAULT_USER_AGENT.parse().unwrap());
 
     if let Some(custom_headers) = custom_headers {
         for (key, value) in custom_headers {
@@ -36,40 +67,41 @@ pub async fn download(
     app: AppHandle,
     url: &str,
     dest_path: &str,
-    custom_headers: Option<Vec<(&str, &str)>>,
+    headers: Option<Vec<(String, String)>>,
 ) -> Result<(), Error> {
     let dest_path = Path::new(dest_path);
-    let headers = parse_headers(custom_headers);
+    let headers = parse_headers(headers);
 
     if let Some(parent) = dest_path.parent() {
         tokio::fs::create_dir_all(parent).await?;
     }
 
-    let response = ReqwestClient::new()
-        .redirect(Policy::limited(3))
-        .build()?
-        .get(url)
-        .headers(headers.clone())
-        .send()
-        .await?;
-    let content_length = response
-        .content_length()
-        .ok_or_else(|| Error::Other("Unable to parse content length".to_string()))?;
     let mut downloaded_bytes = if let Some(range) = headers.get("Range") {
-        info!("Download resumed for {url}");
         range.to_str().unwrap()[6..range.len() - 1]
             .parse::<u64>()
             .unwrap_or(0)
     } else {
         0
     };
+
+    let response = ReqwestClient::new()
+        .redirect(Policy::limited(3))
+        .build()?
+        .get(url)
+        .headers(headers)
+        .send()
+        .await?;
+    let content_length = response
+        .content_length()
+        .ok_or_else(|| Error::Other("Unable to parse content length".to_string()))?;
     let content_length = downloaded_bytes + content_length;
-    let _ = DownloadStartedEvent {
+    DownloadEvent::Started {
         url: url.to_string(),
         path: dest_path.display().to_string(),
         content_length,
     }
-    .emit(&app);
+    .emit(&app)
+    .ok();
 
     let start_time = Instant::now();
     let mut current_iteration: u32 = 0;
@@ -97,10 +129,11 @@ pub async fn download(
                 if abort_download == url {
                     writer.shutdown().await?;
                     tokio::fs::remove_file(dest_path).await?;
-                    let _ = DownloadAbortedEvent {
+                    DownloadEvent::Aborted {
                         url: url.to_string(),
                     }
-                    .emit(&app);
+                    .emit(&app)
+                    .ok();
                     download_aborted = true;
                     state.abort_download = None;
                     break;
@@ -119,22 +152,24 @@ pub async fn download(
             } else {
                 0
             };
-            let progress = downloaded_bytes as f64 / content_length as f64 * 100.0;
-            let _ = DownloadProgressEvent {
+            let progress_percentage = downloaded_bytes as f64 / content_length as f64 * 100.0;
+            DownloadEvent::Progress {
                 url: url.to_string(),
-                progress,
+                progress_percentage,
                 downloaded_bytes,
                 download_speed,
                 eta,
             }
-            .emit(&app);
+            .emit(&app)
+            .ok();
             if let Some(pause_download) = &state.pause_download {
                 if pause_download == url {
                     writer.shutdown().await?;
-                    let _ = DownloadPausedEvent {
+                    DownloadEvent::Paused {
                         url: url.to_string(),
                     }
-                    .emit(&app);
+                    .emit(&app)
+                    .ok();
                     download_paused = true;
                     state.pause_download = None;
                     break;
@@ -145,69 +180,27 @@ pub async fn download(
     }
     writer.flush().await?;
     if !download_aborted && !download_paused {
-        let _ = DownloadCompletedEvent {
+        DownloadEvent::Completed {
             url: url.to_string(),
         }
-        .emit(&app);
-    }
-    info!(
-        "Download {} for {url}",
-        if download_aborted {
-            "aborted"
-        } else if download_paused {
-            "paused"
-        } else {
-            "completed"
-        }
-    );
-    Ok(())
-}
-
-#[tauri::command]
-#[specta::specta]
-pub async fn resume_download(
-    state: State<'_, Mutex<AppState>>,
-    app: AppHandle,
-    url: &str,
-    dest_path: &str,
-    downloaded_bytes: u64,
-) -> Result<(), Error> {
-    download(
-        state,
-        app,
-        url,
-        dest_path,
-        Some(vec![("Range", &format!("bytes={downloaded_bytes}-"))]),
-    )
-    .await
-}
-
-#[tauri::command]
-#[specta::specta]
-pub async fn abort_download(
-    state: State<'_, Mutex<AppState>>,
-    url: Option<String>,
-) -> Result<(), Error> {
-    let mut state = state.lock().await;
-    if let Some(url) = url {
-        state.abort_download = Some(url);
-    } else {
-        state.abort_download = None
+        .emit(&app)
+        .ok();
     }
     Ok(())
 }
 
 #[tauri::command]
 #[specta::specta]
-pub async fn pause_download(
-    state: State<'_, Mutex<AppState>>,
-    url: Option<String>,
-) -> Result<(), Error> {
+pub async fn abort_download(state: State<'_, Mutex<AppState>>, url: String) -> Result<(), Error> {
     let mut state = state.lock().await;
-    if let Some(url) = url {
-        state.pause_download = Some(url);
-    } else {
-        state.pause_download = None
-    }
+    state.abort_download = Some(url);
+    Ok(())
+}
+
+#[tauri::command]
+#[specta::specta]
+pub async fn pause_download(state: State<'_, Mutex<AppState>>, url: String) -> Result<(), Error> {
+    let mut state = state.lock().await;
+    state.pause_download = Some(url);
     Ok(())
 }
