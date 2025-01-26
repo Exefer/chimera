425568c08f19fb5de720c21f8f8b1eeae40e83ac
Aryxst <jessieodeh08@gmail.com>
2025-01-26 12:34:34 +0100

    feat: improve download status display and add app version
    The last download added to the queue will be displayed

--- src/lib/components/bottom-panel.svelte
+++ src/lib/components/bottom-panel.svelte
@@ -1,26 +1,34 @@
 <script lang="ts">
- import { BYTES_TO_MEGABYTES } from "@/constants";
+ import { BYTES_TO_MEGABYTES } from "@/constants/";
  import { downloads } from "@/stores";
  import { formatDuration, intervalToDuration } from "date-fns";
+
+ const downloadsCount = $derived($downloads.length);
+ const currentDownload = $derived($downloads[downloadsCount - 1]);
 </script>
 
 <footer
- class="fixed bottom-0 z-50 w-full border-y bg-background px-4 py-1 text-xs text-muted-foreground shadow"
+ class="fixed bottom-0 z-50 flex w-full justify-between border-y bg-background px-4 py-1 text-xs text-muted-foreground shadow"
 >
  <a class="underline-offset-1 hover:underline" href="/downloads">
-  {#if $downloads[0] && $downloads[0].status == "progress"}
-   {@const download = $downloads[0]}
-
-   Downloading <b>{download.title}</b>...({download.progress?.toFixed(1)}%
-   complete) - Completion in about {formatDuration(
-    intervalToDuration({
-     start: 0,
-     end: (download.eta! || 1) * 1000,
-    }),
-    { format: ["minutes", "seconds", "hours", "days", "weeks"] },
-   )} - {(download.download_speed! / BYTES_TO_MEGABYTES).toFixed(1)}MB/S
+  {#if currentDownload && currentDownload.status == "progress"}
+   Downloading <b>{currentDownload.title}</b
+   >...({currentDownload.progress_percentage?.toFixed(1)}% complete) -
+   {#if currentDownload.eta}
+    Completion in about {formatDuration(
+     intervalToDuration({
+      start: 0,
+      end: currentDownload.eta * 1000,
+     }),
+     { format: ["minutes", "seconds", "hours", "days", "weeks"] },
+    )} - {(currentDownload.download_speed! / BYTES_TO_MEGABYTES).toFixed(1)}MB/S
+   {:else}
+    Calculating ETA...
+   {/if}
   {:else}
    No downloads in progress
   {/if}
  </a>
+
+ <p>Version: {__APP_VERSION__}</p>
 </footer>
