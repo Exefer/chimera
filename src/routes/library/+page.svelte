37ffc83ab07f2fa1b75cc685b0f3686a436dadcc
Aryxst <jessieodeh08@gmail.com>
2025-02-01 12:27:51 +0100

    feat: add a minimal library page

---
+++ src/routes/library/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { steamImageBuilder } from "@/services/steam";
+  import { games } from "@/stores";
+  import { getGameDetailsUrl } from "@/utils";
+</script>
+
+<main class="p-4">
+  <ul class="flex flex-row flex-wrap gap-4">
+    {#each $games as game}
+      <li>
+        <a href={getGameDetailsUrl(game.remote_id, game.title)}>
+          <img
+            class="relative rounded-md"
+            src={steamImageBuilder.cover(game.remote_id)}
+            alt={game.title}
+            width="200"
+          />
+        </a>
+      </li>
+    {/each}
+  </ul>
+</main>
