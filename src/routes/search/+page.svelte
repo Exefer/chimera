c206bce92c0d5113c1eca3e0bc3834bcf14216bf
Aryxst <jessieodeh08@gmail.com>
2025-02-01 12:33:34 +0100

    feat: add i18n and use new game details url func

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,84 +1,85 @@
 <script lang="ts">
- import * as Pagination from "@/components/ui/pagination";
- import { ITEMS_PER_PAGE } from "@/constants/";
- import { apps, isTyping, search } from "@/stores";
- import uFuzzy from "@leeoniya/ufuzzy";
- // See https://github.com/leeoniya/uFuzzy#options
- const uf = new uFuzzy({
-  intraMode: 0,
-  intraIns: 1,
-  interIns: Infinity,
-  intraChars: "[a-z\d\' ]",
-  interChars: ".",
-  interLft: 0,
-  interRgt: 0,
-  intraSub: 1,
-  intraTrn: 1,
-  intraDel: 1,
- });
+  import * as Pagination from "@/components/ui/pagination";
+  import { ITEMS_PER_PAGE } from "@/constants/";
+  import { apps, isTyping, search } from "@/stores";
+  import { getGameDetailsUrl } from "@/utils";
+  import uFuzzy from "@leeoniya/ufuzzy";
+  import { t } from "svelte-i18n";
+  // See https://github.com/leeoniya/uFuzzy#options
+  const uf = new uFuzzy({
+    intraMode: 0,
+    intraIns: 1,
+    interIns: Infinity,
+    intraChars: "[a-z\d\' ]",
+    interChars: ".",
+    interLft: 0,
+    interRgt: 0,
+    intraSub: 1,
+    intraTrn: 1,
+    intraDel: 1,
+  });
 
- const haystack = $apps.map(entry => entry.name);
- const searchResults = $derived.by(() => {
-  if (!$search) return [];
-  const idxs = uf.filter(haystack, $search)!;
+  const haystack = $apps.map(app => app.name);
+  const searchResults = $derived.by(() => {
+    if (!$search) return [];
+    const idxs = uf.filter(haystack, $search)!;
 
-  return idxs.map(index => $apps[index]);
- });
-
- let currentPage = $state<number>(1);
+    return idxs.map(index => $apps[index]);
+  });
+  let currentPage = $state<number>(1);
 </script>
 
-<div class="p-4">
- {#if searchResults && searchResults.length > 0 && !$isTyping}
-  {@const totalPages = Math.ceil(searchResults.length / ITEMS_PER_PAGE)}
-  {@const startIndex = (currentPage - 1) * ITEMS_PER_PAGE}
-  {@const end =
-   currentPage == totalPages ? searchResults.length : startIndex + ITEMS_PER_PAGE}
-  <div class="flex flex-col">
-   {#each searchResults.slice(startIndex, end) as item}
-    <a href="/game/?id={item.id}&title={item.name}">
-     {item.name}
-    </a>
-   {/each}
-  </div>
-  <Pagination.Root
-   count={searchResults.length}
-   perPage={ITEMS_PER_PAGE}
-   bind:page={currentPage}
-  >
-   {#snippet children({ pages, currentPage })}
-    <span
-     ><span class="font-bold">{startIndex == 0 ? 1 : startIndex}-{end}</span>
-     <span>of {searchResults.length}</span></span
+<main class="p-4">
+  {#if searchResults && searchResults.length > 0 && !$isTyping}
+    {@const totalPages = Math.ceil(searchResults.length / ITEMS_PER_PAGE)}
+    {@const startIndex = (currentPage - 1) * ITEMS_PER_PAGE}
+    {@const end =
+      currentPage == totalPages ? searchResults.length : startIndex + ITEMS_PER_PAGE}
+    <div class="flex flex-col">
+      {#each searchResults.slice(startIndex, end) as item}
+        <a href={getGameDetailsUrl(String(item.id), item.name)}>
+          {item.name}
+        </a>
+      {/each}
+    </div>
+    <Pagination.Root
+      count={searchResults.length}
+      perPage={ITEMS_PER_PAGE}
+      bind:page={currentPage}
     >
-    <Pagination.Content class="mt-2">
-     <Pagination.Item>
-      <Pagination.PrevButton />
-     </Pagination.Item>
-     {#each pages as page (page.key)}
-      {#if page.type === "ellipsis"}
-       <Pagination.Item>
-        <Pagination.Ellipsis />
-       </Pagination.Item>
-      {:else}
-       <Pagination.Item>
-        <Pagination.Link {page} isActive={currentPage === page.value}>
-         {page.value}
-        </Pagination.Link>
-       </Pagination.Item>
-      {/if}
-     {/each}
-     <Pagination.Item>
-      <Pagination.NextButton />
-     </Pagination.Item>
-    </Pagination.Content>
-   {/snippet}
-  </Pagination.Root>
- {:else if searchResults.length == 0 && !$isTyping}
-  <div class="flex h-full flex-col items-center justify-center gap-4">
-   <p class="text-muted-foreground">No results found</p>
-  </div>
- {:else if $isTyping}
-  Typing...
- {/if}
-</div>
+      {#snippet children({ pages, currentPage })}
+        <span
+          ><span class="font-bold">{startIndex === 0 ? 1 : startIndex}-{end}</span>
+          <span>of {searchResults.length}</span></span
+        >
+        <Pagination.Content class="mt-2">
+          <Pagination.Item>
+            <Pagination.PrevButton />
+          </Pagination.Item>
+          {#each pages as page (page.key)}
+            {#if page.type === "ellipsis"}
+              <Pagination.Item>
+                <Pagination.Ellipsis />
+              </Pagination.Item>
+            {:else}
+              <Pagination.Item>
+                <Pagination.Link {page} isActive={currentPage === page.value}>
+                  {page.value}
+                </Pagination.Link>
+              </Pagination.Item>
+            {/if}
+          {/each}
+          <Pagination.Item>
+            <Pagination.NextButton />
+          </Pagination.Item>
+        </Pagination.Content>
+      {/snippet}
+    </Pagination.Root>
+  {:else if searchResults.length == 0 && !$isTyping}
+    <div class="flex h-full flex-col items-center justify-center gap-4">
+      <p class="text-muted-foreground">{$t("search.no_results")}</p>
+    </div>
+  {:else if $isTyping}
+    {$t("search.typing")}
+  {/if}
+</main>
