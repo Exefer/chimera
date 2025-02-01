2bdf0e5b8f30ea74cc5716597fad4981c2289c05
Aryxst <jessieodeh08@gmail.com>
2025-02-01 11:50:37 +0100

    feat: internazionalize

--- src/lib/components/bottom-panel.svelte
+++ src/lib/components/bottom-panel.svelte
@@ -1,34 +1,46 @@
 <script lang="ts">
- import { BYTES_TO_MEGABYTES } from "@/constants/";
- import { downloads } from "@/stores";
- import { formatDuration, intervalToDuration } from "date-fns";
+  import { BYTES_TO_MEGABYTES } from "@/constants/";
+  import { downloads } from "@/stores";
+  import { formatDuration, intervalToDuration } from "date-fns";
+  import { t } from "svelte-i18n";
 
- const downloadsCount = $derived($downloads.length);
- const currentDownload = $derived($downloads[downloadsCount - 1]);
+  const [currentDownload] = $derived(
+    $downloads.toSorted((a, b) => b.progress_percentage! - a.progress_percentage!)
+  );
 </script>
 
 <footer
- class="fixed bottom-0 z-50 flex w-full justify-between border-y bg-background px-4 py-1 text-xs text-muted-foreground shadow"
+  class="fixed bottom-0 z-50 flex w-full justify-between border-y bg-background px-4 py-1 text-xs text-muted-foreground shadow"
 >
- <a class="underline-offset-1 hover:underline" href="/downloads">
-  {#if currentDownload && currentDownload.status == "progress"}
-   Downloading <b>{currentDownload.title}</b
-   >...({currentDownload.progress_percentage?.toFixed(1)}% complete) -
-   {#if currentDownload.eta}
-    Completion in about {formatDuration(
-     intervalToDuration({
-      start: 0,
-      end: currentDownload.eta * 1000,
-     }),
-     { format: ["minutes", "seconds", "hours", "days", "weeks"] },
-    )} - {(currentDownload.download_speed! / BYTES_TO_MEGABYTES).toFixed(1)}MB/S
-   {:else}
-    Calculating ETA...
-   {/if}
-  {:else}
-   No downloads in progress
-  {/if}
- </a>
+  <a href="/downloads" class="underline-offset-1 hover:underline">
+    {#if currentDownload && currentDownload.status == "progress"}
+      {#if currentDownload.eta}
+        {$t("bottom_panel.downloading", {
+          values: {
+            title: currentDownload.title,
+            percentage: currentDownload.progress_percentage?.toFixed(1),
+            eta: formatDuration(
+              intervalToDuration({
+                start: 0,
+                end: currentDownload.eta * 1000,
+              }),
+              { format: ["minutes", "seconds", "hours", "days", "weeks"] }
+            ),
+            speed: (currentDownload.download_speed! / BYTES_TO_MEGABYTES).toFixed(1),
+          },
+        })}
+      {:else}
+        {$t("bottom_panel.calculating_eta", {
+          values: {
+            title: currentDownload.title,
+            percentage: currentDownload.progress_percentage?.toFixed(1),
+          },
+        })}
+      {/if}
+    {:else}
+      {$t("bottom_panel.no_downloads_in_progress")}
+    {/if}
+  </a>
 
- <p>Version: {__APP_VERSION__}</p>
+  <p>Version: {__APP_VERSION__}</p>
 </footer>
--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -1,72 +1,74 @@
 <script lang="ts">
- import { beforeNavigate, goto } from "$app/navigation";
- import { page } from "$app/state";
- import { Input } from "@/components/ui/input";
- import { SEARCH_DEBOUNCE } from "@/constants/";
- import { isTyping, search } from "@/stores";
+  import { beforeNavigate, goto } from "$app/navigation";
+  import { page } from "$app/state";
+  import { Input } from "@/components/ui/input";
+  import { SEARCH_DEBOUNCE } from "@/constants/";
+  import { isTyping, search } from "@/stores";
+  import { t } from "svelte-i18n";
 
- let currentSearch = $state<string>("");
+  const title = () => {
+    switch (page.url.pathname) {
+      case "/":
+        return $t("header.home");
+      case "/search":
+        return $t("header.search_results");
+      case "/downloads":
+        return $t("header.downloads");
+      case "/library":
+        return $t("header.library");
+      case "/settings":
+        return $t("header.settings");
+      case "/catalog":
+        return $t("header.catalog");
+      case "/game-details":
+        return page.url.searchParams.get("title")!;
+      default:
+        return document.title;
+    }
+  };
 
- const title = () => {
-  switch (page.url.pathname) {
-   case "/":
-    return "Home";
-   case "/catalogue":
-    return "Catalogue";
-   case "/search":
-    return "Search Results";
-   case "/downloads":
-    return "Downloads";
-   case "/library":
-    return "Library";
-   case "/settings":
-    return "Settings";
-   case "/game":
-    return page.url.searchParams.get("title")!;
-   default:
-    return document.title;
-  }
- };
+  beforeNavigate(() => {
+    if (page.url.pathname != "/search") return;
+    currentSearch = "";
+  });
 
- beforeNavigate(() => {
-  if (!(page.url.pathname == "/search")) return;
-  currentSearch = "";
- });
-
- let timeout: NodeJS.Timeout | null = null;
+  let currentSearch = $state("");
+  let timeout: NodeJS.Timeout | null = null;
 </script>
 
 <header
- class="sticky top-0 z-10 flex justify-between border-b border-b-border bg-header px-4 py-4"
+  class="sticky top-0 z-10 flex justify-between border-b border-b-border bg-header px-4 py-4"
 >
- <h1 class="self-center text-lg font-bold">
-  {title()}
- </h1>
- <div>
-  <Input
-   autocomplete="off"
-   placeholder="Search Apps"
-   bind:value={currentSearch}
-   oninput={() => {
-    if (!currentSearch) {
-     goto("/", { keepFocus: true });
-     return;
-    }
-    if (currentSearch == $search) return;
+  <h1 class="self-center text-lg font-bold">
+    {title()}
+  </h1>
+  <div>
+    <Input
+      type="text"
+      name="search"
+      placeholder={$t("header.search")}
+      autocomplete="off"
+      bind:value={currentSearch}
+      oninput={() => {
+        if (!currentSearch) {
+          goto("/", { keepFocus: true });
+          return;
+        }
+        if (currentSearch == $search) return;
 
-    if (page.url.pathname != "/search") {
-     goto("/search", { keepFocus: true });
-    }
+        if (page.url.pathname != "/search") {
+          goto("/search", { keepFocus: true });
+        }
 
-    if (timeout) clearTimeout(timeout);
+        if (timeout) clearTimeout(timeout);
 
-    isTyping.set(true);
+        isTyping.set(true);
 
-    timeout = setTimeout(() => {
-     isTyping.set(false);
-     search.set(currentSearch);
-    }, SEARCH_DEBOUNCE);
-   }}
-  />
- </div>
+        timeout = setTimeout(() => {
+          isTyping.set(false);
+          search.set(currentSearch);
+        }, SEARCH_DEBOUNCE);
+      }}
+    />
+  </div>
 </header>
