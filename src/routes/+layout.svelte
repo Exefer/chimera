78f4296364e716d2cd11612a3edf17fe0779a762
Aryxst <jessieodeh08@gmail.com>
2025-02-01 12:18:13 +0100

    feat: init i18n, persist downloads state on window close and improve layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
- import { dev } from "$app/environment";
- import AppSidebar from "@/components/app-sidebar.svelte";
- import BottomPanel from "@/components/bottom-panel.svelte";
- import Header from "@/components/header";
- import * as Sidebar from "@/components/ui/sidebar";
- import { events } from "@/specta-bindings";
- import { games, settings } from "@/stores";
- import { ModeWatcher } from "mode-watcher";
- import { onMount } from "svelte";
- import { Toaster } from "svelte-sonner";
- import "../app.css";
+  import { dev } from "$app/environment";
+  import AppSidebar from "@/components/app-sidebar.svelte";
+  import BottomPanel from "@/components/bottom-panel.svelte";
+  import Header from "@/components/header";
+  import * as Sidebar from "@/components/ui/sidebar";
+  import { events } from "@/specta-bindings";
+  import { downloads, games, settings } from "@/stores";
+  import * as Persistent from "@/stores/persistent";
+  import { sleep } from "@/utils";
+  import { getCurrentWindow } from "@tauri-apps/api/window";
+  import { ModeWatcher } from "mode-watcher";
+  import { onMount, untrack } from "svelte";
+  import { init, locale, register, t } from "svelte-i18n";
+  import { toast, Toaster } from "svelte-sonner";
+  import "../app.css";
 
- let { children } = $props();
+  let { children } = $props();
 
   $effect.pre(() => {
     register("en", () => import("../locales/en.json"));
@@ -25,48 +29,75 @@
 
     untrack(() => locale.set($settings.general.locale));
   });
-     games.updateGame(["executablePath", data.path], state => ({
-      ...state,
-      running: true,
-     }));
-     break;
-    }
-    case "finished": {
-     games.updateGame(["executablePath", data.path], state => ({
-      ...state,
-      lastPlayedAt: Date.now(),
-      playtimeInSeconds: state.playtimeInSeconds + data.execution_time,
-      running: false,
-     }));
-     break;
-    }
-   }
+
+  onMount(() => {
+    const appWindow = getCurrentWindow();
+
+    appWindow.once("tauri://close-requested", () => {
+      Promise.all(
+        $downloads.map(async download => {
+          if (download.status != "progress") return;
+          toast.info(
+            $t("layout.pausing_download_for", {
+              values: { title: download.title },
+            })
+          );
+          await downloads.pauseDownload(download.url);
+        })
+      ).then(async () => {
+        toast.info($t("layout.saving_downloads"));
+        await Persistent.downloads.set($downloads);
+        toast.info($t("layout.closing"));
+        await sleep(1000);
+        await appWindow.close();
+      });
+    });
+
+    events.executableEvent.listen(({ payload: { data, type } }) => {
+      switch (type) {
+        case "started": {
+          games.updateGame("executable_path", data.path, state => ({
+            ...state,
+            running: true,
+          }));
+          break;
+        }
+        case "finished": {
+          games.updateGame("executable_path", data.path, state => ({
+            ...state,
+            last_played_at: Date.now(),
+            playtime_in_seconds: state.playtime_in_seconds + data.execution_time,
+            running: false,
+          }));
+          break;
+        }
+      }
+    });
   });
- });
 </script>
 
-<Toaster theme={$settings.theme} />
+<Toaster theme={$settings.general.theme} />
 <ModeWatcher />
 
-<BottomPanel />
-<Sidebar.Provider style="--sidebar-width: 14rem; --sidebar-width-mobile: 20rem;">
- <AppSidebar />
- <main class="w-full">
-  <Header />
-  <aside>{@render children()}</aside>
- </main>
+<Sidebar.Provider>
+  <AppSidebar />
+  <div class="w-full">
+    <Header />
+    {@render children()}
+  </div>
+  <BottomPanel />
 </Sidebar.Provider>
 
 <svelte:window
- on:contextmenu|preventDefault
- on:keydown={event => {
-  if (dev) return;
-  if (
-   event.key === "F5" ||
-   (event.ctrlKey && event.key === "r") ||
-   (event.metaKey && event.key === "r")
-  ) {
-   event.preventDefault();
-  }
- }}
+  on:contextmenu|preventDefault
+  on:keydown={event => {
+    if (dev) return;
+    if (
+      event.key === "F5" ||
+      (event.ctrlKey && event.key === "r") ||
+      (event.metaKey && event.key === "r")
+    ) {
+      event.preventDefault();
+    }
+  }}
 />
