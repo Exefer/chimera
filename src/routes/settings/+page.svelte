09446ccd5ad26933384316fed067be34d0f10d69
Aryxst <jessieodeh08@gmail.com>
2025-02-01 12:34:00 +0100

    feat: add i18n and improve layout

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,45 +1,42 @@
 <script lang="ts">
- import { Button } from "@/components/ui/button";
- import Behaviour from "./behavior.svelte";
- import General from "./general.svelte";
- import Sources from "./sources.svelte";
+  import { Button } from "@/components/ui/button";
+  import { t } from "svelte-i18n";
+  import Behavior from "./behavior.svelte";
+  import General from "./general.svelte";
+  import Sources from "./sources.svelte";
 
- const tabs = [
-  {
-   name: "General",
-   component: General,
-  },
-  {
-   name: "Behaviour",
-   component: Behaviour,
-  },
-  {
-   name: "Sources",
-   component: Sources,
-  },
- ];
+  const tabs = [
+    {
+      name: () => $t("settings.general:title"),
+      component: General,
+    },
+    {
+      name: () => $t("settings.behavior:title"),
+      component: Behavior,
+    },
+    {
+      name: () => $t("settings.sources:title"),
+      component: Sources,
+    },
+  ];
 
- let selected = $state(tabs[0]);
+  let selected = $state(tabs[0]);
 </script>
 
-<div class="p-4">
- <div class="flex h-fit flex-col gap-4 rounded-lg border bg-background p-6">
-  <div class="flex gap-2">
-   {#each tabs as tab}
-    <Button
-     variant={tab.name == selected.name ? "default" : "outline"}
-     onclick={() => {
-      selected = tab;
-     }}>{tab.name}</Button
-    >
-   {/each}
+<main class="p-4">
+  <div
+    class="flex h-fit flex-col gap-4 rounded-lg border p-6 shadow-sm dark:bg-background"
+  >
+    <div class="flex gap-2">
+      {#each tabs as tab}
+        <Button
+          variant={tab.name() == selected.name() ? "default" : "outline"}
+          onclick={() => {
+            selected = tab;
+          }}>{tab.name()}</Button
+        >
+      {/each}
+    </div>
+    <selected.component />
   </div>
-  <selected.component />
- </div>
-</div>
-
-<style>
- :global(button[role="checkbox"] + label) {
-  font-weight: normal;
- }
-</style>
+</main>
