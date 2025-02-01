ca3e361927b0560138750494ae9799e1b6ca077c
Aryxst <jessieodeh08@gmail.com>
2025-02-01 12:24:54 +0100

    feat: add downloads page with grouped download status view

---
+++ src/routes/downloads/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { downloads } from "@/stores";
+  import DownloadIcon from "lucide-svelte/icons/download";
+  import { t } from "svelte-i18n";
+  import DownloadGroup from "./download-group.svelte";
+
+  const downloadGroups = $derived([
+    {
+      title: $t("downloads.download_in_progress"),
+      items: $downloads.filter(download => download.status == "progress"),
+    },
+    {
+      title: $t("downloads.queued_downloads"),
+      items: $downloads.filter(download => download.status == "paused"),
+    },
+    {
+      title: $t("downloads.completed"),
+      items: $downloads.filter(download => download.status == "completed"),
+    },
+  ]);
+</script>
+
+<main
+  class={[
+    "flex min-h-[calc(100vh-99px)] p-4",
+    { "items-center justify-center": $downloads.length == 0 },
+  ]}
+>
+  {#if $downloads.length > 0}
+    <div class="flex w-full flex-col gap-4">
+      {#each downloadGroups as group}
+        <DownloadGroup {...group} />
+      {/each}
+    </div>
+  {:else}
+    <div class="flex flex-col items-center justify-center gap-4 p-4">
+      <DownloadIcon size="48" />
+      <p class="text-lg font-bold">{$t("downloads.no_downloads")}</p>
+      <p class="text-sm text-muted-foreground">
+        {$t("downloads.no_downloads:description")}
+      </p>
+    </div>
+  {/if}
+</main>
---
+++ src/routes/downloads/download-group.svelte
@@ -0,0 +1,120 @@
+<script lang="ts">
+  import * as DropdownMenu from "@/components/ui/dropdown-menu";
+  import { DOWNLOADER_NAME } from "@/constants";
+  import { steamImageBuilder } from "@/services/steam";
+  import { downloads } from "@/stores";
+  import * as Types from "@/types";
+  import { formatBytes, getGameDetailsUrl } from "@/utils";
+  import CirclePlay from "lucide-svelte/icons/circle-play";
+  import CircleX from "lucide-svelte/icons/circle-x";
+  import DownloadIcon from "lucide-svelte/icons/download";
+  import Menu from "lucide-svelte/icons/menu";
+  import Pause from "lucide-svelte/icons/pause";
+  import { t } from "svelte-i18n";
+  import { toast } from "svelte-sonner";
+
+  interface DownloadGroupProps {
+    title: string;
+    items: Types.Download[];
+  }
+
+  let { title, items }: DownloadGroupProps = $props();
+</script>
+
+{#if items.length > 0}
+  <section>
+    <div
+      class="mb-4 flex flex-row items-center justify-between gap-4 text-muted-foreground"
+    >
+      <h2 class="text-2xl font-bold">{title}</h2>
+      <hr class="h-[1px] flex-1 bg-border" />
+      <span class="text-lg font-semibold">
+        {items.length}
+      </span>
+    </div>
+    <ul class="flex flex-col gap-2">
+      {#each items as download (download.url)}
+        {@const packer = DOWNLOADER_NAME[download.downloader]}
+        <li class="relative flex h-40 flex-row rounded-md border">
+          <div class="relative w-[300px]">
+            <img
+              src={steamImageBuilder.library(download.remote_id)}
+              alt={download.title}
+              class="size-full rounded-l-md border-r object-fill"
+            />
+            <span
+              class="absolute bottom-2 right-2 rounded-md border bg-background px-2 py-1 text-xs"
+            >
+              {packer}
+            </span>
+          </div>
+          <div class="flex w-2/3 flex-col justify-center gap-2 p-4">
+            <a
+              href={getGameDetailsUrl(download.remote_id, download.title)}
+              class="text-lg font-medium hover:underline"
+            >
+              {download.title}
+            </a>
+            <div class="text-sm text-muted-foreground">
+              {#if download.status == "progress"}
+                <p>{download.progress_percentage?.toFixed(1)}%</p>
+                <p>
+                  {formatBytes(download.downloaded_bytes!)} / {formatBytes(
+                    download.content_length
+                  )}
+                </p>
+              {:else if download.status == "paused"}
+                <p>{$t("downloads.paused")}</p>
+              {:else if download.status == "completed"}
+                <p>{$t("downloads.completed")}</p>
+              {/if}
+            </div>
+          </div>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger
+              class="absolute right-3 top-3 rounded-full p-1.5 transition-colors hover:bg-muted"
+              ><Menu /></DropdownMenu.Trigger
+            >
+            <DropdownMenu.Content side="bottom" align="end">
+              <DropdownMenu.Item
+                onclick={() => {
+                  if (download.status == "paused") {
+                    downloads.resumeDownload(download.url);
+                  } else if (download.status == "progress") {
+                    downloads.pauseDownload(download.url);
+                  } else {
+                    toast.info($t("common.not_implemented"));
+                  }
+                }}
+              >
+                {#if download.status == "paused"}
+                  <CirclePlay /> {$t("downloads.resume")}
+                {:else if download.status == "progress"}
+                  <Pause /> {$t("downloads.pause")}
+                {:else}
+                  <DownloadIcon /> {$t("downloads.install")}
+                {/if}
+              </DropdownMenu.Item>
+              <DropdownMenu.Item
+                onclick={async () => {
+                  if (download.status == "completed") {
+                    downloads.deleteDownload(download.url);
+                  } else {
+                    downloads.abortDownload(download.url);
+                  }
+                }}
+              >
+                <CircleX />
+                {#if download.status == "completed"}
+                  <p>{$t("downloads.delete")}</p>
+                {:else}
+                  <p>{$t("downloads.abort")}</p>
+                {/if}
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+        </li>
+      {/each}
+    </ul>
+  </section>
+{/if}
