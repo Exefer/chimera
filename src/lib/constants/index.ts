f96f38583b850ab4bdb347cb07e7265e6676463f
Aryxst <jessieodeh08@gmail.com>
2025-02-01 11:51:20 +0100

    refactor: restructure app settings and add enums for downloader and content descriptors

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,6 +1,21 @@
 import * as Types from "@/types";
 export { CHAR_MAP } from "./char-map";
 
+export enum Downloader {
+  Torrent,
+  Gofile,
+  RealDebrid,
+  Unknown,
+}
+
+export enum SteamContentDescriptor {
+  SomeNudityOrSexualContent = 1,
+  FrequenceViolenceOrGore = 2,
+  AdultOnlySexualContent = 3,
+  FrequentNudityOrSexualContent = 4,
+  GeneralMatureContent = 5,
+}
+
 export const BYTES_TO_MEGABYTES = 1024 * 1024;
 
 export const SEARCH_DEBOUNCE = 250;
@@ -8,21 +23,25 @@ export const SEARCH_DEBOUNCE = 250;
 export const ITEMS_PER_PAGE = 20;
 
 export const DEFAULT_APP_SETTINGS: Types.AppSettings = {
- // General
- downloadsPath: "",
- theme: "system",
- locale: "en",
- // General - Notifications
- notifyOnDownloadComplete: false,
- // Behavior
- launchMinimized: false,
- launchOnStartup: false,
- minimizeToTray: false,
+  general: {
+    downloads_path: "",
+    theme: "system",
+    locale: "en",
+    notifications: {
+      when_download_complete: false,
+    },
+  },
+  behavior: {
+    launch_minimized: false,
+    launch_on_startup: false,
+    minimize_to_tray: false,
+    disable_nsfw_alert: false,
+  },
 };
 
-export const DOWNLOADER_NAMES: Record<Types.Downloader, string> = {
- [Types.Downloader.Gofile]: "Gofile",
- [Types.Downloader.RealDebrid]: "Real-Debrid",
- [Types.Downloader.Torrent]: "Torrent",
- [Types.Downloader.Unknown]: "Generic HTTP",
+export const DOWNLOADER_NAME: Record<Downloader, string> = {
+  [Downloader.Gofile]: "Gofile",
+  [Downloader.RealDebrid]: "Real-Debrid",
+  [Downloader.Torrent]: "Torrent",
+  [Downloader.Unknown]: "Generic HTTP",
 };
