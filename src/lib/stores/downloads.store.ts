3b5686d2b6c21ca001e39fd28b55d329de594637
Aryxst <jessieodeh08@gmail.com>
2025-02-01 12:04:16 +0100

    feat: add persistence, improve download management and add gofile downloading

--- src/lib/stores/downloads.store.ts
+++ src/lib/stores/downloads.store.ts
@@ -1,168 +1,218 @@
+import { Downloader } from "@/constants";
 import { GoFileApi } from "@/services/hosters/gofile";
-import { type DownloadEvent, commands, events } from "@/specta-bindings";
+import { commands, events } from "@/specta-bindings";
 import * as Types from "@/types";
 import { getDownloaderFromUrl } from "@/utils";
 import { get, writable } from "svelte/store";
-
-type StatusValues = Exclude<
- DownloadEvent extends { type: infer T } ? T : never,
- "started"
->;
-type ProgressEvent = Extract<DownloadEvent, { type: "progress" }>;
-type Download = Partial<ProgressEvent["data"]> & {
- title: string;
- url: string;
- remote_id: string;
- downloader: Types.Downloader;
- content_length: number;
- status: StatusValues;
- path?: string;
-};
+import * as Persistent from "./persistent";
 
 function createDownloadsStore() {
- const store = writable<Download[]>([]);
+  const store = writable<Types.Download[]>([]);
+  Persistent.downloads.get().then(downloads => {
+    if (downloads) store.set(downloads);
+  });
 
- const addDownload = async (
-  url: string,
-  remoteId: string,
-  title: string,
-  path: string,
- ) => {
-  const state = get(store);
-  if (state.find(download => download.url == url)) return;
+  /**
+   *  Adds a new download and sets up a one-time listener for its start event.
+   */
+  const addDownload = async (
+    url: string,
+    remoteId: string,
+    title: string,
+    path: string
+  ) => {
+    const state = get(store);
+    const download = state.find(download => download.url == url);
+    if (download) return;
 
-  const downloader = getDownloaderFromUrl(url);
+    const downloader = getDownloaderFromUrl(url);
 
-  const listener = events.downloadEvent.listen(({ payload }) => {
-   switch (payload.type) {
-    case "started": {
-     store.update(state => {
-      console.log("Payload: ", payload.data);
-      state.push({
-       ...payload.data,
-       title,
-       downloader,
-       remote_id: remoteId,
-       status: "progress",
-      });
+    const listener = events.downloadEvent.listen(({ payload }) => {
+      switch (payload.type) {
+        case "started": {
+          Promise.all(
+            state.map(async download => await commands.pauseDownload(download.url))
+          ).then(() => {
+            store.update(state => {
+              state.push({
+                ...payload.data,
+                original_url: url,
+                title,
+                downloader,
+                remote_id: remoteId,
+                status: "progress",
+                progress_percentage: 0,
+                downloaded_bytes: 0,
+              });
+
+              return state;
+            });
+          });
+
+          listener.then(unlisten => unlisten());
+          break;
+        }
+      }
+    });
+
+    switch (downloader) {
+      case Downloader.Gofile: {
+        const token = await GoFileApi.authorize();
+        const link = await GoFileApi.getDownloadLink(url.split("/").pop()!);
+        const filename = link.split("/").pop()!;
+        commands.download(link, `${path}/${filename}`, [
+          ["Cookie", `accountToken=${token}`],
+        ]);
+        break;
+      }
+      default: {
+        // TODO: Implement download for other downloaders
+        break;
+      }
+    }
+  };
+
+  /**
+   * Listen for download events from the backend and update store state accordingly.
+   * Events include progress updates, completion, pausing and aborting.
+   */
+  events.downloadEvent.listen(({ payload: { type, data } }) => {
+    switch (type) {
+      case "progress": {
+        store.update(state => {
+          const index = state.findIndex(download => download.url == data.url);
+          state.splice(index, 1, { ...state[index], ...data });
+
+          return state;
+        });
+        break;
+      }
+      case "completed": {
+        store.update(state => {
+          const index = state.findIndex(download => download.url == data.url);
+          const download = state[index];
+          // Clean up progress-related fields on completion
+          delete download["downloaded_bytes"];
+          delete download["download_speed"];
+          delete download["progress_percentage"];
+          delete download["eta"];
+          state.splice(index, 1, {
+            ...download,
+            ...data,
+            status: type,
+          });
+
+          return state;
+        });
+        break;
+      }
+      case "paused": {
+        store.update(state => {
+          const index = state.findIndex(download => download.url == data.url);
+          state.splice(index, 1, { ...state[index], ...data, status: type });
+
+          return state;
+        });
+        break;
+      }
+      case "aborted": {
+        store.update(state => {
+          const index = state.findIndex(download => download.url == data.url);
+          state.splice(index, 1);
+
+          return state;
+        });
+        break;
+      }
+    }
+  });
+
+  /**
+   * Request the backend to pause a download.
+   *
+   * The pause is not instantaneous - the backend will continue downloading
+   * until the next progress check interval before actually pausing.
+   * A "paused" event will be emitted when the download is actually paused (max 2 seconds delay).
+   */
+  const pauseDownload = commands.pauseDownload;
+
+  /**
+   * Request the backend to abort a download.
+   *
+   * The abort this is not instantaneous - the backend will stop at the next progress check.
+   * The downloaded file will be deleted and an "aborted" event will be emitted.
+   */
+  const abortDownload = (url: string) => {
+    commands.abortDownload(url);
+    store.update(state => {
+      const index = state.findIndex(download => download.url == url);
+      state.splice(index, 1);
 
       return state;
-     });
-     listener.then(unlisten => unlisten());
-     break;
+    });
+  };
+
+  /**
+   * Resume a paused download.
+   *
+   * Uses the Range header to continue from where the download was paused.
+   * The download will restart with a "progress" status.
+   */
+  const resumeDownload = async (url: string) => {
+    const download = get(store).find(download => download.url == url);
+    if (!download) return;
+
+    switch (download.downloader) {
+      case Downloader.Gofile: {
+        const token = await GoFileApi.authorize();
+        const link = await GoFileApi.getDownloadLink(
+          download.original_url.split("/").pop()!
+        );
+        commands.download(link, download.path!, [
+          ["Range", `bytes=${download.downloaded_bytes}-`],
+          ["Cookie", `accountToken=${token}`],
+        ]);
+        break;
+      }
+      default: {
+        // TODO: Implement resume download for other downloaders
+        break;
+      }
     }
-   }
-  });
-
-  switch (downloader) {
-   case Types.Downloader.Torrent: {
-    // TODO: Implement torrent download
-    break;
-   }
-   case Types.Downloader.Gofile: {
-    const token = await GoFileApi.authorize();
-    const link = await GoFileApi.getDownloadLink(url.split("/").pop()!);
-    const filename = link.split("/").pop()!;
-    console.log(`${path}/${filename}`);
-    commands.download(link, `${path}/${filename}`, [["Cookie", `accountToken=${token}`]]);
-
-    break;
-   }
-   case Types.Downloader.RealDebrid: {
-    // TODO: Implement RealDebrid download
-    break;
-   }
-   case Types.Downloader.Unknown: {
-    // TODO: Implement unknown download
-    break;
-   }
-  }
- };
-
- events.downloadEvent.listen(({ payload: { type, data } }) => {
-  switch (type) {
-   case "progress": {
     store.update(state => {
-     const index = state.findIndex(download => download.url == data.url);
-     state.splice(index, 1, { ...state[index], ...data });
+      const index = state.findIndex(download => download.url == url);
+      const download = state[index];
+      state.splice(index, 1, { ...download, status: "progress" });
 
-     return state;
+      return state;
     });
-    break;
-   }
-   case "completed": {
+  };
+
+  const deleteDownload = async (url: string) => {
+    const download = get(store).find(download => download.url == url);
+    if (!download) return;
+
+    const existsFile = await commands.exists(download.path!);
+    if (existsFile) {
+      await commands.deleteFile(download.path!);
+    }
+
     store.update(state => {
-     const index = state.findIndex(download => download.url == data.url);
-     const download = state[index];
-     delete download["downloaded_bytes"];
-     delete download["download_speed"];
-     delete download["progress_percentage"];
-     delete download["eta"];
-     state.splice(index, 1, {
-      ...download,
-      ...data,
-      status: type,
-     });
+      const index = state.findIndex(download => download.url == url);
+      state.splice(index, 1);
 
-     return state;
+      return state;
     });
-    break;
-   }
-   case "paused": {
-    store.update(state => {
-     const index = state.findIndex(download => download.url == data.url);
-     state.splice(index, 1, { ...state[index], ...data, status: type });
+  };
 
-     return state;
-    });
-    break;
-   }
-   case "aborted": {
-    store.update(state => {
-     const index = state.findIndex(download => download.url == data.url);
-     state.splice(index, 1);
-
-     return state;
-    });
-    break;
-   }
-  }
- });
-
- const pauseDownload = commands.pauseDownload;
-
- const abortDownload: (
-  ...args: Parameters<typeof commands.abortDownload>
- ) => void = url => {
-  commands.abortDownload(url);
-  store.update(state => {
-   const index = state.findIndex(download => download.url == url);
-   state.splice(index, 1);
-
-   return state;
-  });
- };
-
- const resumeDownload = (url: string) => {
-  store.update(state => {
-   const index = state.findIndex(download => download.url == url);
-   const download = state[index];
-   commands.download(url, download.path!, [
-    ["Range", `bytes=${download.downloaded_bytes}-`],
-   ]);
-   state.splice(index, 1, { ...download, status: "progress" });
-
-   return state;
-  });
- };
-
- return {
-  ...store,
-  addDownload,
-  pauseDownload,
-  resumeDownload,
-  abortDownload,
- };
+  return {
+    ...store,
+    addDownload,
+    pauseDownload,
+    resumeDownload,
+    abortDownload,
+    deleteDownload,
+  };
 }
 
 export const downloads = createDownloadsStore();
