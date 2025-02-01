6b2d4af4f5a0e0e90975ce2ba4bde5d0778ce601
Aryxst <jessieodeh08@gmail.com>
2025-02-01 12:09:26 +0100

    feat: make downloads persistent

--- src/lib/stores/persistent.ts
+++ src/lib/stores/persistent.ts
@@ -4,22 +4,29 @@ import { LazyStore } from "@tauri-apps/plugin-store";
 const store = new LazyStore("store.json", { autoSave: true });
 
 export const settings = {
- get: () => store.get<Types.AppSettings>("settings"),
- set: async (value: Types.AppSettings) => {
-  await store.set("settings", value);
- },
+  get: () => store.get<Types.AppSettings>("settings"),
+  set: async (value: Types.AppSettings) => {
+    await store.set("settings", value);
+  },
 };
 
 export const games = {
- get: () => store.get<Types.Game[]>("games"),
- set: async (value: Types.Game[]) => {
-  await store.set("games", value);
- },
+  get: () => store.get<Types.Game[]>("games") ?? [],
+  set: async (value: Types.Game[]) => {
+    await store.set("games", value);
+  },
 };
 
 export const sources = {
- get: () => store.get<string[]>("sources"),
- set: async (value: string[]) => {
-  await store.set("sources", value);
- },
+  get: () => store.get<string[]>("sources") ?? [],
+  set: async (value: string[]) => {
+    await store.set("sources", value);
+  },
+};
+
+export const downloads = {
+  get: () => store.get<Types.Download[]>("downloads") ?? [],
+  set: async (value: Types.Download[]) => {
+    await store.set("downloads", value);
+  },
 };
