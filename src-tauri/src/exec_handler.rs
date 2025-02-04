c6a3cef6c7eb9f83ead2a29733475aa869e5f339
Aryxst <jessieodeh08@gmail.com>
2025-02-04 11:16:44 +0100

    refactor: refactor the shortcut creation function to compile conditionally entirely

--- src-tauri/src/exec_handler.rs
+++ src-tauri/src/exec_handler.rs
@@ -12,7 +12,7 @@ use tauri::{AppHandle, Manager};
 use tauri_specta::Event;
 
 #[derive(Serialize, Deserialize, Debug, Clone, Type, Event)]
-#[serde(rename_all = "lowercase", tag = "type", content = "data")]
+#[serde(rename_all = "snake_case", tag = "type", content = "data")]
 pub enum ExecutableEvent {
     Started { path: String },
     Finished { path: String, execution_time: u32 },
@@ -49,50 +49,48 @@ pub fn run_executable(app: AppHandle, path: String) -> Result<(), Error> {
 
 #[tauri::command]
 #[specta::specta]
+#[cfg(target_os = "windows")]
 pub fn create_shortcut(app: AppHandle, target_path: String) -> Result<(), Error> {
-    #[cfg(target_os = "windows")]
+    // Path to the PowerShell script
+    let ps_path = app
+        .path()
+        .resolve(
+            "scripts/create_shortcut.ps1",
+            tauri::path::BaseDirectory::Resource,
+        )
+        .unwrap();
+    // Get the shortcut name from the target path
+    let shortcut_name = target_path
+        .rsplit('\\')
+        .next()
+        .unwrap_or_default()
+        .replace(".exe", ".lnk");
+    // Use PowerShell to execute the script
+    match Command::new("powershell")
+        .args([
+            "-ExecutionPolicy",
+            "Bypass",
+            "-File",
+            &ps_path.display().to_string(),
+            "-TargetPath",
+            &target_path,
+            "-ShortcutName",
+            &shortcut_name,
+        ])
+        .output()
     {
-        // Path to the PowerShell script
-        let ps_path = app
-            .path()
-            .resolve(
-                "scripts/create_shortcut.ps1",
-                tauri::path::BaseDirectory::Resource,
-            )
-            .unwrap();
-        // Get the shortcut name from the target path
-        let shortcut_name = target_path
-            .rsplit('\\')
-            .next()
-            .unwrap_or_default()
-            .replace(".exe", ".lnk");
-        // Use PowerShell to execute the script
-        match Command::new("powershell")
-            .args([
-                "-ExecutionPolicy",
-                "Bypass",
-                "-File",
-                &ps_path.display().to_string(),
-                "-TargetPath",
-                &target_path,
-                "-ShortcutName",
-                &shortcut_name,
-            ])
-            .output()
-        {
-            Ok(output) => {
-                if output.status.success() {
-                    println!("Shortcut created successfully!");
-                } else {
-                    eprintln!(
-                        "Error creating shortcut: {}",
-                        String::from_utf8_lossy(&output.stderr)
-                    );
-                }
+        Ok(output) => {
+            if output.status.success() {
+                println!("Shortcut created successfully!");
+            } else {
+                eprintln!(
+                    "Error creating shortcut: {}",
+                    String::from_utf8_lossy(&output.stderr)
+                );
             }
-            Err(e) => {
-                eprintln!("Failed to execute PowerShell script: {}", e);
-            }
+        }
+        Err(e) => {
+            eprintln!("Failed to execute PowerShell script: {}", e);
         }
     }
     Ok(())
