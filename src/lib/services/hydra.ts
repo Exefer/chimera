ade7e03594f72500cd3a8f0b099c28c182eddbe0
Aryxst <jessieodeh08@gmail.com>
2025-02-16 10:12:52 +0100

    feat: add hydra api class with static methods for interacting with their API

---
+++ src/lib/services/hydra.ts
@@ -0,0 +1,23 @@
+import { CatalogueCategory } from "@/constants";
+import { fetch } from "@tauri-apps/plugin-http";
+import ky from "ky";
+
+export class HydraApi {
+  private static instance = ky.create({
+    prefixUrl: import.meta.env.VITE_HYDRA_API_URL,
+    fetch,
+  });
+
+  public static getCatalogue(category: CatalogueCategory) {
+    const searchParams = new URLSearchParams({
+      take: "12",
+      skip: "0",
+    });
+
+    return this.instance
+      .get<
+        Array<{ title: string; shop: "steam" | "epic"; objectId: string }>
+      >(`catalogue/${category}?${searchParams.toString()}`)
+      .json();
+  }
+}
